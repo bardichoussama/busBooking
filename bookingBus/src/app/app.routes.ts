37b99fe55c2b6accf74d94c5be80ba4bd51044e7
brcOne <amv.0280@gmail.com>
2025-04-16 13:46:27 +0100

    add pages

--- bookingBus/src/app/app.routes.ts
+++ bookingBus/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { BookTicketComponent } from './pages/book-ticket/book-ticket.component';
+import { MyBookingsComponent } from './pages/my-bookings/my-bookings.component';
+import { SearchResultComponent } from './pages/search-result/search-result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+       path : "",
+       redirectTo : "search",
+       pathMatch: "full"
+    },
+   {
+    path : "search",
+    component : SearchComponent
+   },
+   {
+    path : "search-result",
+    component : SearchResultComponent
+   },
+   {
+    path : "book-ticket",
+    component : BookTicketComponent
+   },
+   {
+    path : "my-bookings",
+    component : MyBookingsComponent
+   },
+   
+
+];
---
+++ bookingBus/src/app/pages/search/search.component.ts
@@ -0,0 +1,12 @@
+import { Component,inject,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-search',
+  imports: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent  {
+
+}
